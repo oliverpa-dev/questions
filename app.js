a2e08dc6ee4f1fbde6630f74a4bcef9b48e15848
oliver.paszkiewicz@interia.pl <oliverpaszkiewicz@livewallgroup.com>
2022-06-04 18:41:12 +0200

    Add: array of questions for the application

--- app.js
+++ app.js
@@ -1,1 +1,29 @@
 'use strict';
+
+// array with questions
+const quizData = [
+    {
+        question: 'How old is Oliver?',
+        a: '10',
+        b: '17',
+        c: '26',
+        d: '110',
+        correct:  'c'
+    },
+    {
+        question: 'What is the best programming language?',
+        a: 'js',
+        b: 'c++',
+        c: 'c#',
+        d: 'ruby',
+        correct:  'b'
+    },
+    {
+        question: 'Who is the President of Poland?',
+        a: 'Kaczyński',
+        b: 'Duda',
+        c: 'Morawiecki',
+        d: 'Trzaskowski',
+        correct: 'b'
+    }
+]
