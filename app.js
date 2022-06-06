07ecffdffb0cb0cca02318660f026a0f37d2a958
oliver.paszkiewicz@interia.pl <oliverpaszkiewicz@livewallgroup.com>
2022-06-06 14:39:11 +0200

    Add: styling for the modal and logic for the questionarie

--- app.js
+++ app.js
@@ -24,6 +24,43 @@ const quizData = [
         b: 'Duda',
         c: 'Morawiecki',
         d: 'Trzaskowski',
-        correct: 'b'
+        correct: 'b',
     }
 ]
+
+// index for the question
+let currentQuestion = 0;
+
+
+// html elements with id of 'a,b,c,d-text
+const submitEl = document.getElementById('submit');
+const questionEl = document.getElementById('question');
+const a_text = document.getElementById('a_text');
+const b_text = document.getElementById('b_text');
+const c_text = document.getElementById('c_text');
+const d_text = document.getElementById('d_text');
+
+// function where questions are going to be loaded
+loadQuiz();
+
+// function for loading the question together with the answers
+function loadQuiz() {
+    const currentQuiz = quizData[currentQuestion];
+    questionEl.innerText = currentQuiz.question;
+    a_text.innerText = currentQuiz.a;
+    b_text.innerText = currentQuiz.b;
+    c_text.innerText = currentQuiz.c;
+    d_text.innerText = currentQuiz.d;
+}
+
+submitEl.addEventListener('click', () => {
+   currentQuestion++;
+
+    if(currentQuestion < quizData.length) {
+        loadQuiz(); 
+    } else {
+        currentQuestion = 0;
+        alert('You have finished the questionarie');
+    }
+
+});
